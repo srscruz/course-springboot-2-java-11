d7462edeac4cb53744b98413f5501edb92302305
srscruz <srscruz13@gmail.com>
2020-02-28 19:03:48 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/sages4it/course/service/OrderService.java
@@ -0,0 +1,26 @@
+package com.sages4it.course.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sages4it.course.entities.Order;
+import com.sages4it.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+
+	private OrderRepository repository;
+
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	public Order findById(long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
