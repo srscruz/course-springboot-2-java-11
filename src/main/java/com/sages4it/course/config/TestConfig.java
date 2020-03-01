3091b275b4aac79d3a395350ee88eec1e45f7ded
srscruz <srscruz13@gmail.com>
2020-02-29 21:24:15 -0300

    Payment, one-to-one association

--- src/main/java/com/sages4it/course/config/TestConfig.java
+++ src/main/java/com/sages4it/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.sages4it.course.entities.Category;
 import com.sages4it.course.entities.Order;
 import com.sages4it.course.entities.OrderItem;
+import com.sages4it.course.entities.Payment;
 import com.sages4it.course.entities.Product;
 import com.sages4it.course.entities.User;
 import com.sages4it.course.entities.enums.OrderStatus;
@@ -78,6 +79,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T20:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
