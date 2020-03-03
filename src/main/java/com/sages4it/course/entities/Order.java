fe89e5c3ef3fc80da37b88e90a0c5714468367fe
srscruz <srscruz13@gmail.com>
2020-03-03 15:28:14 -0300

    Subtotal & Total methods

--- src/main/java/com/sages4it/course/entities/Order.java
+++ src/main/java/com/sages4it/course/entities/Order.java
@@ -98,6 +98,13 @@ public class Order implements Serializable {
 	public 	Set<OrderItem> getItems(){
 		return items;
 	}
+	public Double getTotal() {
+		double sum = 0.00;
+		for(OrderItem x: items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
 	
 	@Override
 	public int hashCode() {
--- src/main/java/com/sages4it/course/entities/Payment.java
+++ src/main/java/com/sages4it/course/entities/Payment.java
@@ -11,6 +11,8 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 @Table(name = "tb_payment")
 public class Payment implements Serializable {
@@ -20,7 +22,8 @@ public class Payment implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Instant moment;
-	@OneToOne
+	@JsonIgnore	
+	@OneToOne    
 	@MapsId
 	private Order order;
 	
