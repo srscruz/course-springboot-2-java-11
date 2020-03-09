72a6224ec1ab0f38d0b9f3440364e8e8f350e6f0
srscruz <srscruz13@gmail.com>
2020-03-09 20:40:43 -0300

    Exception Hendling - delete

--- src/main/java/com/sages4it/course/service/UserService.java
+++ src/main/java/com/sages4it/course/service/UserService.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.sages4it.course.entities.User;
 import com.sages4it.course.repositories.UserRepository;
+import com.sages4it.course.resource.exceptions.DatabaseException;
+import com.sages4it.course.service.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +26,7 @@ public class UserService {
 
 	public User findById(long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
@@ -30,9 +34,15 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
-
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+		throw new ResourceNotFoundException(id); 
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
+
 	public User update(Long id, User obj) {
 		User entity = repository.getOne(id);
 		updateData(entity, obj);
