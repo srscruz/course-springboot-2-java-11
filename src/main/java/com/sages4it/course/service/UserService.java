75b0a25e084dd1d45e8f4cf1b59c98d8e4e5c914
srscruz <srscruz13@gmail.com>
2020-03-09 20:50:42 -0300

    Exception Hendling - update

--- src/main/java/com/sages4it/course/service/UserService.java
+++ src/main/java/com/sages4it/course/service/UserService.java
@@ -3,6 +3,8 @@ package com.sages4it.course.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -37,16 +39,21 @@ public class UserService {
 		try {
 			repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
-		throw new ResourceNotFoundException(id); 
-		}catch (DataIntegrityViolationException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
